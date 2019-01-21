fc9b43a6d310a3c7c20707a9b2a05f0dd1ac5b40
ShisukeUrahara <abhisheksagu@gmail.com>
2019-01-21 20:29:17 +0530

    Update firebase.js

--- scripts/utils/firebase.js
+++ scripts/utils/firebase.js
@@ -1,12 +1,14 @@
 
 
   // Initialize Firebase
+
+// your firebase code here
   var config = {
-    apiKey: "AIzaSyCbVg8ZtoU9j75ZDzjFmYYWCiWrKKBw1nI",
-    authDomain: "testengine-680f4.firebaseapp.com",
-    databaseURL: "https://testengine-680f4.firebaseio.com",
-    projectId: "testengine-680f4",
-    storageBucket: "testengine-680f4.appspot.com",
-    messagingSenderId: "580805754746"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   };
   firebase.initializeApp(config);
